ad91ab2de7ae9d2620654c631c8112e678f1a8be
Albertus Antok Sudaryanto <antok.acb@gmail.com>
2021-01-28 10:41:47 +0700

    upload
    localhost apache

--- app/Config/App.php
+++ app/Config/App.php
@@ -1,4 +1,6 @@
-<?php namespace Config;
+<?php
+
+namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
 
@@ -21,8 +23,8 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost:8080/';
-
+	// public $baseURL = 'http://localhost:8080/';
+	public $baseURL = BASE;
 	/*
 	|--------------------------------------------------------------------------
 	| Index File
